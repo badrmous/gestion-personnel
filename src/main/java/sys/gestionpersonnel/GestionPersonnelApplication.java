0f3d7e73b55966a24b938bba4e10329e87fdee1b
unknown <b.moustaghfir@brome.ma>
2023-01-18 18:17:07 +0100

    first commit

---
+++ src/main/java/sys/gestionpersonnel/GestionPersonnelApplication.java
@@ -0,0 +1,15 @@
+package sys.gestionpersonnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GestionPersonnelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPersonnelApplication.class, args);
+	}
+
+}
